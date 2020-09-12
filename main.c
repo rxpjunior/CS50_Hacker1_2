290e1433e6f08daa88a64591b429201c8eef8eee
rxpjunior <rxpjunior@gmail.com>
2020-09-12 16:45:45 -0300

    Testando a operadora do cartao

--- main.c
+++ main.c
@@ -5,8 +5,11 @@
 
 int main()
 {
+//Primeiro bloco - entrada do valor e validação
 printf("Programa de validacao de numero de cartao de credito\n");
 int valida1;
+char cartao[256];
+char bandeira[256]="";
 do{
     valida1=0; //Variavel para validacao de entrada
     char entrada[256];
@@ -31,5 +34,31 @@ do{
         printf("\n\nEntrada incorreta: caracter(es) invalido(s): %s\n",mensagem); // exibe os caracteres invalidos da entrada
 
     }
+    //Verifica se aoperadora é valida
+    else{
+        char temp[3]=" ";
+        int op;
+        for (int cont=0;cont<2;cont++){
+        temp[cont]=entrada[cont];
+       }
+       op = atoi(temp);
+       if(op < 34 || op > 55){
+            printf("\n\nOperadora incorreta. Tente novamente!\n");
+            valida1++;
+       }
+       else if ((temp[0]-'0') == 4){
+            strcpy(bandeira,"Visa");
+       }else if(op == 34 || op == 37){
+            strcpy(bandeira,"AMEX");
+       }else if(op > 50 && op < 56){
+            strcpy(bandeira,"Master Card");
+       }else{
+            printf("\n\nOperadora incorreta. Tente novamente!\n");
+            valida1++;
+       }
+    }
+    printf("operadora %s",bandeira);
+    fflush(stdin); //limpa as entradas
 }while(valida1>0);
+
 }
